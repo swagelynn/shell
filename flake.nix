65d8d196906ba54d52416723d099f8291496d82c
s4midev <astingraye@proton.me>
2025-09-26 23:41:56 +0100

    update flake inputs to use fork

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     caelestia-cli = {
-      url = "github:caelestia-dots/cli";
+      url = "github:s4midev/cli";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.caelestia-shell.follows = "";
     };
